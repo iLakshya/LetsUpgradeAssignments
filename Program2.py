c9e0c7739097356e90cffac90874e1f05ded06c0
Lakshya Goel <lgoel340@gmail.com>
2022-09-06 09:52:41 +0530

    Program2.py

---
+++ Program2.py
@@ -0,0 +1,19 @@
+# Basics of *args and **kwargs work
+# Sum of integers using *args
+def my_sum(*args):
+    result = 0
+    # Iterating over the Python args tuple
+    for x in args:
+        result += x
+    return result
+print(my_sum(1, 2, 3))
+
+
+# Concatenate using **kwargs
+def concatenate(**kwargs):
+    result = ""
+    # Iterating over the Python kwargs dictionary
+    for arg in kwargs.values():
+        result += arg
+    return result
+print(concatenate(a="Real", b="Python", c="Is", d="Great", e="!"))
