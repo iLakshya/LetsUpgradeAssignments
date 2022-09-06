8795ca20e0bbcd1bea23d410fe6838ac199c1dc4
Lakshya Goel <lgoel340@gmail.com>
2022-09-06 10:24:02 +0530

    Project

---
+++ Project.py
@@ -0,0 +1,10 @@
+# Project to roll the dice
+import random
+while True:
+    print("1. Roll the dice 2. To Exit")
+    user = int(input("what you want to do\n"))
+    if user==1:
+        number = random.randint(1,6)
+        print(number)
+    else:
+        break
