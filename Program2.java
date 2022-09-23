f3209160b237f4517f6090aedbfc4ff0724e26b9
Lakshya Goel <lgoel340@gmail.com>
2022-09-24 01:48:49 +0530

    Program 2

---
+++ Program2.java
@@ -0,0 +1,13 @@
+public class Program2
+{
+    public static void main(String args[])
+    {
+        int n = 50;
+        int sum = 0;
+        for(int i=1;i<=n;i++)
+        {
+            sum = sum + i;
+        }
+        System.out.println("Sum of 50 natural number is: " + sum);
+    }   
+}
