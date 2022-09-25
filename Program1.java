ae4e8b2e54f8d878e72926e3d492c755b3cebbd4
Lakshya Goel <lgoel340@gmail.com>
2022-09-25 22:52:58 +0530

    Program 1

--- Program1.java
+++ Program1.java
@@ -4,12 +4,19 @@ public class Program1
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter two numbers: ");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int temp = a;
-        a = b;
-        b = temp;
-        System.out.println("Swapped Values are: "+ a + b);
+        try
+        {
+            System.out.println("Enter two numbers: ");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int temp = a;
+            a = b;
+            b = temp;
+            System.out.println("Swapped Values are: "+ a + b);
+        }
+        finally
+        {
+            sc.close();
+        }
     }
 }
