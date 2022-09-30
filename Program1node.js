f12dd8a5ab09bc5131d0b1a7c5784514d32a8b7e
Lakshya Goel <lgoel340@gmail.com>
2022-09-30 15:47:42 +0530

    Program 1

---
+++ Program1node.js
@@ -0,0 +1,7 @@
+const http = require("http");
+const server = http.createServer((req,res)=>{
+    res.end('Hello Guys!!!');
+});
+server.listen(8000,"127.0.0.1",()=>{
+    console.log("listening to the port number 8000");
+});
